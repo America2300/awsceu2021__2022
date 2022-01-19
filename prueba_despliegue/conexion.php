971d1a1d2076f093d6ed68c4ee9f32f8f4e0146c
Félix Chong <america.felix@isepceu.es>
2022-01-19 15:41:25 +0100

    Initial commit

---
+++ prueba_despliegue/conexion.php
@@ -0,0 +1,23 @@
+<?php
+//leo el fichero de claves
+$string = file_get_contents(".claves");
+
+$lineas = explode(" ",$string);
+
+//datos de la conexión
+$bbdd=$lineas[0]; //la base de datos es la linea 0
+$usuario=$lineas[1];//el usuario es la linea 1
+$clave=$lineas[2];//la clave es la linea 3
+//Conexión mediante PDO
+$opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+
+//Conexión mediante PDO
+try {
+    $db = new PDO("mysql:host=localhost;dbname=$bbdd", $usuario, $clave,$opciones);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Falló la conexión: ' . $e->getMessage();
+}
+
+//Comprobación de la versión
+echo $db->getAttribute(PDO::ATTR_SERVER_VERSION);
